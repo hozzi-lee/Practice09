80a6c506fdb152606a0c6d0c365ed20200a03b69
leehojin-93 <hogenie93@gmail.com>
2021-06-17 00:22:49 +0900

    21. 06. 17. Thu) 00:22 Practice09 incomplete

---
+++ src/com/javaex/ex03/Goods.java
@@ -0,0 +1,59 @@
+package com.javaex.ex03;
+
+public class Goods {
+
+	// field
+	private String name;
+	private int price;
+	private int count;
+	private int allCount;
+
+
+
+	// constructors
+	public Goods() {
+		super();
+	}
+	public Goods(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+		this.allCount += count;
+	}
+
+	
+
+	// method getter/setter
+	public int allCount() {
+		return allCount;
+	}
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getPrice() {
+		return price;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public int getCount() {
+		return count;
+	}
+
+	// method
+	public void showInfo() {
+		System.out.println( name + "(가격:" + price + "원)이 " + count + " 개 입고 되었습니다.");
+	}
+
+
+}
---
+++ src/com/javaex/ex03/GoodsApp.java
@@ -0,0 +1,36 @@
+package com.javaex.ex03;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		List<Goods> gList = new ArrayList<Goods>();
+		while (true) {
+			String goods = scan.nextLine();
+			if ( goods.equals("q") ) {
+				System.out.println("입력완료");
+				System.out.println("==============================");
+				for ( Goods gg : gList ) {
+					gg.showInfo();
+				}
+				// System.out.println("모든 상품의 개수는 " + 미완성 + "개입니다.");
+				break;
+			}
+			String[] goodsArray = goods.split(",");
+			int price = Integer.parseInt(goodsArray[1]);
+			int count = Integer.parseInt(goodsArray[2]);
+			Goods g = new Goods(goodsArray[0], price, count);
+			gList.add(g);
+			
+		}
+
+		scan.close();
+
+	}
+
+}
